f2de5ac385d9d250ac3dfbcd8e8b5d041047a1e8
DESKTOP-NUOPUIF\ITPS <ghrn7989@daum.net>
2021-03-15 17:19:05 +0900

    최초 프로젝트 업로드

---
+++ src/Chap01/Ex01.java
@@ -0,0 +1,14 @@
+package Chap01;
+
+//소스 코드의 최소 단위
+public class Ex01 {
+	//소스코드의 시작점
+	public static void main(String[] args) {
+		
+		//출력 기능
+		//()안에다 출력 대상을 지정
+		System.out.println("Hello  World");
+		System.out.println("주석 연습 중...");
+	}
+
+}
---
+++ src/Chap01/Ex02.java
@@ -0,0 +1,30 @@
+package Chap01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+//		System.out.println("Hello  World~!");
+//		System.out.println("I am");
+//		System.out.println("a");
+//		System.out.println("Java Programmer");
+		
+//		System.out.print("오늘은 ");
+//		System.out.print("자바 수업 ");
+//		System.out.print("첫 번째 날입니다");
+		
+//		System.out.print("I am ");
+//		System.out.print("a ");
+//		System.out.print("Java Programmer");
+		
+//		System.out.println(1);
+//		System.out.println(3.14);
+//		
+//		System.out.println('a');
+//		
+//		System.out.println("abcd");
+		
+		System.out.println("-오늘의 기온: "+-5.3);
+		System.out.println("-커피 가격: "+1500);
+		System.out.println("-철수의 Java 과목 학점: "+'A');
+	}
+
+}
